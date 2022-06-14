a8f107a354d13cb2e19ccfd315dbd9ef7f44d7b7
sitay0490 <107504133+sitay0490@users.noreply.github.com>
2022-06-14 21:15:13 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { buildTree } from "./common/common";
+import { appendix } from "./data";
+import Tree from "./components/Tree";
+import { useState } from "react";
+import { DataIssue } from "./components/types";
+
+function App() {
+  const [issuesList, setIssuesList] = useState<DataIssue[]>([]);
+
+  const NodeClickHandler = (issues: DataIssue[]) => {
+    setIssuesList(issues);
+  };
+
+  return (
+    <div style={{ marginLeft: 15 }}>
+      <Tree node={buildTree(appendix)} onNodeClick={NodeClickHandler} />
+      {issuesList && (
+        <ul>
+          {issuesList?.map((issue, index) => {
+            return (
+              <li key={index}>
+                {}
+                {`component: ${issue.component} selector: ${issue.selector} type:${issue.type}`}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      <span>Ⓒ Itay Sorin Ⓒ</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/types.tsx
@@ -0,0 +1,23 @@
+export interface DataIssue {
+  readonly type: string;
+  readonly severity: string;
+  readonly component: string;
+  readonly selector: string;
+}
+
+export interface TreeNode {
+  url: string;
+  parent?: string;
+  children?: TreeNode[];
+  issues?: DataIssue[];
+}
+
+export interface DataIssueList {
+  url: string;
+  issueList: DataIssue[];
+}
+
+export type TreeProps = {
+  onNodeClick: (issues: DataIssue[]) => void;
+  node: TreeNode;
+};
